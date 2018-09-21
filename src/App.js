2fcc3d997d7587c1fc7a72440168f1fa98e8f283
Abigail Swarth <abigailswarth@gmail.com>
2018-09-21 16:25:07 -0400

    updating state

--- src/App.js
+++ src/App.js
@@ -6,26 +6,36 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: "Abby", age: 28 },
-      { name: "Zach", age: 30 }
+      { id: "asd", name: "Abby", age: 28 },
+      { id: "asldfj", name: "Zach", age: 30 }
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    // const persons = this.state.persons.slice();
+    // or (updating state immutably)
+    const persons = [...this.state.persons]
     persons.splice(personIndex, 1);
     this.setState({persons: persons})
   }
 
-  nameChangedHandler = (e) => {
-    this.setState({
-      persons: [
-        { name: "Abigail", age: 28 },
-        { name: e.target.value, age: 30 }
-      ]
-     })
+  nameChangedHandler = (e, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = e.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
   }
 
   togglePersonHandler = () => {
@@ -49,7 +59,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)} />
+            return <Person
+              key={person.id}
+              name={person.name}
+              age={person.age}
+              click={() => this.deletePersonHandler(index)}
+              change={(e) => this.nameChangedHandler(e, person.id)} />
           })}
           {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler}/>
         // <Person name={this.state.persons[1].name} age={this.state.persons[1].age} change={this.nameChangedHandler}/> */}
