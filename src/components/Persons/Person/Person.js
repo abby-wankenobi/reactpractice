11efeaa9ee1fc3365048dab2da0d2b40b29ed5af
Abigail Swarth <abigailswarth@gmail.com>
2018-09-24 16:52:12 -0400

    const to classes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
 
-const person = (props) => {
-
-
-  return (
+class Person extends Component {
+  render () {
+    return (
       <div className={classes.Person}>
-        <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old.</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.change} value={props.name} />
+        <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.change} value={this.props.name} />
       </div>
-  )
-};
+    )
+  };
+}
 
-export default person;
+
+
+export default Person;
